0d9626aa9fdfe7fcec5a64841dcb25882321d620
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-09-18 12:23:03 -0400

    edited trim

--- trim_fastq.sh
+++ trim_fastq.sh
@@ -11,6 +11,22 @@
 #SBATCH --account berglandlab_standard
 #SBATCH --array=1-21%10   # Adjust the range based on the number of folders
 
+# Load necessary modules
+module load gcc htslib
+# module load sratoolkit/3.1.1
+module load trimmomatic
+# module load bwa
+# module load samtools
+#module load picard
+
+:<<download_pear
+download miniconda and then pear: run in SSH terminal: (it will take a long time)
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+    source $HOME/miniconda3/bin/activate
+    conda install -c bioconda pear
+
+download_pear
 
 #mv /scratch/ejy4bu/UK2022_2024/allshortreads/01.RawData/SR* /scratch/rjp5nc/UK2022_2024/allshortreads/01.RawData/SRR/
 
@@ -23,14 +39,15 @@ infq="/scratch/ejy4bu/compBio/fastq"
 sample_folders=($(ls -d ${infq}/*/))  # Array of folder paths
 # Select the folder based on the job array index
 sample_dir="${sample_folders[$SLURM_ARRAY_TASK_ID - 1]}"
+#sample_dir="/scratch/ejy4bu/compBio/fastq/SRR14476638"
 # Extract folder name
 samp_name=$(basename "$sample_dir")
 
 # Change to the working directory
 cd "$sample_dir" || exit 1
 
-# Check if fastq files exist
 
+# Check if fastq files exist
 #https://www.baeldung.com/linux/compgen-command-usage
 if compgen -f -G "*.fastq" > /dev/null 2>&1; then
     echo "Processing $samp_name"
@@ -42,9 +59,9 @@ if compgen -f -G "*.fastq" > /dev/null 2>&1; then
         ${samp_name}_1.U.trimm.fastq \
         ${samp_name}_2.P.trimm.fastq \
         ${samp_name}_2.U.trimm.fastq \
-        ILLUMINACLIP:/home/rjp5nc/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
+        ILLUMINACLIP:/home/ejy4bu/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
     # Run PEAR to merge overlapping reads
-    /home/rjp5nc/miniconda3/bin/pear \
+    /home/ejy4bu/miniconda3/bin/pear \
         -f ${samp_name}_1.P.trimm.fastq \
         -r ${samp_name}_2.P.trimm.fastq \
         -o ${samp_name} \
@@ -61,4 +78,35 @@ reverse=${samp_dir}/*_2.fastq
 if ![ -f $forward || -f $reverse ]; then
     echo "Skipping ${samp} (missing fastq files)"
 fi 
+
+
+    trimmomatic PE -threads 10 \ #replaced by ##deleted because installing trimmomatic
+        java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE -threads 10 \
+    ILLUMINACLIP:/home/ejy4bu/miniconda3/bin/trimmomatic/TrimmomaticAdaptors/CombinedPE-PE.fa:2:30:10:8:true
+        
+    #deleted because downloading trimmomatic instead
+        ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:8:true
+
+    /home/ejy4bu/miniconda3/bin/pear \
+
+    # could do:
+
+    export EBROOT_PEAR=/home/ejy4bu/miniconda3
+    $EBROOT_PEAR/bin/pear -f sample_1.P.trimm.fastq -r sample_2.P.trimm.fastq -o sample -j 10
+
+    #didn't work:
+    $CONDA_PREFIX/bin/pear -f sample_1.P.trimm.fastq -r sample_2.P.trimm.fastq -o sample -j 10
+
+    #no pear module on the cluster
+        pear -f sample_1.P.trimm.fastq -r sample_2.P.trimm.fastq -o sample -j 10
+
+
+    #calling PEAR module. 
+        -f ${samp_name}_1.P.trimm.fastq \
+        -r ${samp_name}_2.P.trimm.fastq \
+        -o ${samp_name} \
+        -j 10
+
+
+
 deletes
