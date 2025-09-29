539827aa2727c5f62a6cb1d943e39ba2aca82083
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-09-29 09:21:40 -0400

    increased memory

--- run_kraken.sh
+++ run_kraken.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 0-10:00 # 10 hours
-#SBATCH --mem 100G
+#SBATCH --mem 200G
 #SBATCH -o /scratch/ejy4bu/erroroutputs/down.%A_%a.out # Standard output
 #SBATCH -e /scratch/ejy4bu/erroroutputs/down.%A_%a.err # Standard error
 #SBATCH -p standard
