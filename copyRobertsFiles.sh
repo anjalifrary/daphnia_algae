14d34d9faef20797eb3831c2b1c0730e824e1480
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-10-04 14:46:38 -0400

    extracted copy code to new file

---
+++ copyRobertsFiles.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#SBATCH -J merge_pipeline    # Job name
+#SBATCH --ntasks=1        # Single task per job
+#SBATCH --cpus-per-task=10 # Number of CPU cores per task
+#SBATCH -N 1              # Run on one node
+#SBATCH -t 0-10:00        # 10 hours runtime
+#SBATCH --mem=100G        # Memory per node
+#SBATCH -o /scratch/ejy4bu/erroroutputs/pipe.%A_%a.out  # Standard output
+#SBATCH -e /scratch/ejy4bu/erroroutputs/pipe.%A_%a.err  # Standard error
+#SBATCH -p standard       # Partition
+#SBATCH --account=berglandlab
+
+DATA_DIRS=(
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F1"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F2"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F3"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F4"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F5"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F6"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F7"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F8"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F9"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F10"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F11"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F12"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G1"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G2"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G3"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G4"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G5"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G6"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G7"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G8"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G9"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G10"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G11"
+"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G12"
+)
+mkdir -p "/scratch/ejy4bu/compBio/Robert_samples"
+for samp in "${!DATA_DIRS[@]}"; do
+    samp_dir=${DATA_DIRS[$samp]}
+    name=$(basename "$samp_dir")
+    mkdir -p "/scratch/ejy4bu/compBio/Robert_samples/$name"
+    files=("$samp_dir"/*.fq.gz)
+    if [ ${#files[@]} -eq 0 ]; then
+        echo "No .fq.gz files in $samp_dir, skipping"
+        continue
+    fi
+    cp -n "${files[@]}" "/scratch/ejy4bu/compBio/Robert_samples/${name}"
+    echo "Copied ${#files[@]} files"
+done
--- merge_map_pipeline.sh
+++ merge_map_pipeline.sh
@@ -10,48 +10,6 @@
 #SBATCH -p standard       # Partition
 #SBATCH --account=berglandlab
 
-:<<copy_files
-DATA_DIRS=(
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F1"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F2"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F3"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F4"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F5"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F6"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F7"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F8"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F9"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F10"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F11"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_F12"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G1"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G2"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G3"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G4"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G5"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G6"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G7"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G8"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G9"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G10"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G11"
-"/project/berglandlab/Robert/shortread_data/data/01.RawData/RobertUK_G12"
-)
-mkdir -p "/scratch/ejy4bu/compBio/Robert_samples"
-for samp in "${!DATA_DIRS[@]}"; do
-    samp_dir=${DATA_DIRS[$samp]}
-    name=$(basename "$samp_dir")
-    mkdir -p "/scratch/ejy4bu/compBio/Robert_samples/$name"
-    files=("$samp_dir"/*.fq.gz)
-    if [ ${#files[@]} -eq 0 ]; then
-        echo "No .fq.gz files in $samp_dir, skipping"
-        continue
-    fi
-    cp -n "${files[@]}" "/scratch/ejy4bu/compBio/Robert_samples/${name}"
-    echo "Copied ${#files[@]} files"
-done
-copy_files
-
 MY_DATA="/scratch/ejy4bu/compBio/Robert_samples"
 cd "$MY_DATA"
 
