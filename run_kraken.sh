04cb6d4a8b23fb8d081f70ff9c5f17c6b34b29e8
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-10-06 11:20:02 -0400

    .

--- run_kraken.sh
+++ run_kraken.sh
@@ -35,6 +35,7 @@ samp_name=$(basename ${sample%.sort.bam})
 
 SAMPLE="${samp_path}/${samp_name}/${samp_name}_output.fastq"
 
+
 #convert bam back to fastq
 #samtools fastq -o ${SAMPLE} ${sample}
 if [ ! -f "$SAMPLE" ]; then
