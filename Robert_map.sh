6256d09ccbcfe6939b4ebf04d075985446ebf23e
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-10-09 10:42:09 -0400

    updated cores and memory

--- Robert_map.sh
+++ Robert_map.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #SBATCH -J map_pipeline    # Job name
 #SBATCH --ntasks=1        # Single task per job
-#SBATCH --cpus-per-task=10 # Number of CPU cores per task
+#SBATCH --cpus-per-task=4 # Number of CPU cores per task
 #SBATCH -N 1              # Run on one node
 #SBATCH -t 0-10:00        # 10 hours runtime
-#SBATCH --mem=100G        # Memory per node
+#SBATCH --mem=50G        # Memory per node
 #SBATCH -o /scratch/ejy4bu/erroroutputs/pipe.%A_%a.out  # Standard output
 #SBATCH -e /scratch/ejy4bu/erroroutputs/pipe.%A_%a.err  # Standard error
 #SBATCH -p standard       # Partition
