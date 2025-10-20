1f8e839ee9cba6b8f738519c1a3507762361ed61
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-10-20 10:39:02 -0400

    making vcfs

--- makeVCF.sh
+++ makeVCF.sh
@@ -1,18 +1,43 @@
+#!/usr/bin/env bash
+#
+#SBATCH -J makeVCF # A single job name for the array
+#SBATCH --ntasks-per-node=10 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 0-10:00 # 10 hours
+#SBATCH --mem 100G
+#SBATCH -o /scratch/ejy4bu/erroroutputs/makeVCF/vcf.%A_%a.out # Standard output
+#SBATCH -e /scratch/ejy4bu/erroroutputs/makeVCF/vcf.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+mkdir -p /scratch/ejy4bu/erroroutputs/makeVCF
+
 module load samtools varscan bcftools
-# sbatch --array=1-12 ~/Genoseq_2023/run_varscan_copy.sh
+
+# sbatch --array=1-12 ~/daphnia_algae/makeVCF.sh
 # 40646429
+
+bam_root=/scratch/ejy4bu/compBio/bams
+ref_fasta=/project/berglandlab/daphnia_ref/totalHiCwithallbestgapclosed.fa
+out_vcf=/scratch/ejy4bu/compBio/vcfs
+
+mkdir -p $out_vcf
+
 # Get the chromosome for the current SLURM task
 chr=$(sed -n ${SLURM_ARRAY_TASK_ID}p /scratch/ejy4bu/compBio/genomefiles/ChrScaffoldList)
 echo $chr
+
 # Store BAM file list in a variable to maintain order
-bam_list=$(ls /scratch/rjp5nc/UK2022_2024/allshortreads/sortedbamsdedup/*.bam | sort)
+bam_list=$(ls $bam_root/*/*/*.dedup.bam | sort)
+echo "Using ${#bam_list[@]} BAMs"
+
 # Generate VCF with VarScan
-samtools mpileup \
+samtools mpileup -@ 10 \
     -r ${chr} \
-    --fasta-ref /project/berglandlab/daphnia_ref/totalHiCwithallbestgapclosed.fa \
+    --fasta-ref $ref_fasta \
     $bam_list | \
 java -jar $EBROOTVARSCAN/VarScan.v2.4.4.jar mpileup2snp \
     /dev/stdin \
     --min-coverage 4 \
     --min-var-freq 0.001 \
-    --output-vcf > /scratch/rjp5nc/UK2022_2024/allshortreads/chr/2022seq.${chr}.vcf
+    --output-vcf > $out_vcf/${chr}.vcf
