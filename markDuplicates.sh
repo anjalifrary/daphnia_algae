12c4b045bddcbe80bbf620e51f5ae95fdde7fe22
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-10-23 11:29:46 -0400

    update

--- markDuplicates.sh
+++ markDuplicates.sh
@@ -11,13 +11,35 @@
 
 module load picard
 
-for bam in /scratch/ejy4bu/compBio/bams/*/*/*.sort.bam; do
-    sample=$(basename $bam .sort.bam)
-    outdir=$(dirname $bam)
-    java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-        I=$bam \
-        O=${outdir}/${sample}.dedup.bam \
-        M=${outdir}/${sample}.dedup.metrics \
-        REMOVE_DUPLICATES=true \
-        CREATE_INDEX=true
-done
+bams=(/scratch/ejy4bu/compBio/bams/*/*/*.sort.bam)
+
+bam=${bams[$((SLURM_ARRAY_TASK_ID - 1))]}
+sample=$(basename "$bam" .sort.bam)
+outdir=$(dirname "$bam")
+dedup_bam="${outdir}/${sample}.dedup.bam"
+
+
+echo "Processing $bam"
+
+if [ -f "$dedup_bam" ]; then
+    echo "Skipping $bam (already deduplicated)"
+    exit 0
+fi
+
+java -Xmx45G -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+    I="$bam" \
+    O="$dedup_bam" \
+    M="${outdir}/${sample}.dedup.metrics" \
+    REMOVE_DUPLICATES=true \
+    CREATE_INDEX=true
+
+# for bam in /scratch/ejy4bu/compBio/bams/*/*/*.sort.bam; do
+#     sample=$(basename $bam .sort.bam)
+#     outdir=$(dirname $bam)
+#     java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+#         I=$bam \
+#         O=${outdir}/${sample}.dedup.bam \
+#         M=${outdir}/${sample}.dedup.metrics \
+#         REMOVE_DUPLICATES=true \
+#         CREATE_INDEX=true
+# done
