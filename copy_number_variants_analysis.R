c3cddc34a11cd7a898b4ee08372514f438b01545
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-10 20:59:18 -0500

    normalized depth

--- copy_number_variants_analysis.R
+++ copy_number_variants_analysis.R
@@ -11,14 +11,60 @@ library(doMC)
 registerDoMC(10)
 
 out_dir <- "/scratch/ejy4bu/compBio/bam_analysis/coverage_data"
+metadata_file <- "/scratch/ejy4bu/compBio/bam_analysis/metadata.csv"
+
+meta <- fread(metadata_file)
 
 coverage_files <- list.files(out_dir, pattern = "_5000bp.csv", recursive = TRUE, full.names = TRUE)
 
-coverage <- rbindlist(lapply(coverage_files, fread), use.names=TRUE, fill=TRUE)
+coverage <- rbindlist(lapply(coverage_files, function(f) {
+  dt <- fread(f)
+  dt[, sampleID := sub("_5000bp\\.csv$", "", basename(f))]
+  return(dt)
+}), use.names = TRUE, fill = TRUE)
 
-coverage[, sampleID := tstrsplit(basename(V1), "_5000bp.csv")[[1]]]
+coverage <- merge(coverage, meta[, .(sampleID, algae_group, algae_source, propPulex)], by = "sampleID", all.x = TRUE)
 
-coverage[, algae_source := ifelse(grepl("Old_Algae_bams", coverage_files, ignore.case = TRUE), "UTEX",
-                 ifelse(grepl("Robert_samples_bams", coverage_files, ignore.case = TRUE), "REED",
-                 ifelse(grepl("Sephadex", coverage_files, ignore.case = TRUE), "REED", NA)))
-]
+setorder(coverage, algae_group, sampleID)
+coverage[, sampleID := factor(sampleID, levels = unique(sampleID))]
+coverage[, norm_depth := mean_depth / mean(mean_depth), by = sampleID]
+
+
+
+### plot genome wide
+
+genome_avg <- coverage[, .(avg_depth = mean(norm_depth)), by = .(sampleID, algae_group)]
+
+# Dot plot
+p1 <- ggplot(genome_avg, aes(x = sampleID, y = avg_depth, color = algae_group)) +
+  geom_point(size = 2) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 6)) +
+  labs(x = "Sample", y = "Average Depth", title = "Genome-wide mean depth by sample") +
+  scale_color_manual(values = c("REED_Sephadex"="brown3",
+                                "REED_NotSephadex"="cyan3",
+                                "UTEX"="dodgerblue3"))
+
+ggsave(file.path(out_dir, "genome_wide_coverage.pdf"), p1, width = 20, height = 10)
+
+
+### per chromosome plots
+chr_list <- unique(coverage$chrom)
+
+for(chr in chr_list){
+  chr_data <- coverage[chrom == chr]
+  
+  # Create numeric x-axis along the chromosome (window index)
+    setorder(chr_data, start)
+    chr_data[, window_idx := 1:.N, by = sampleID]
+  
+  p <- ggplot(chr_data, aes(x = window_idx, y = norm_depth, color = algae_group, group = sampleID)) +
+    geom_line() +
+    theme_bw() +
+    labs(x = "Window", y = "Mean Depth", title = paste0("Coverage across ", chr)) +
+    scale_color_manual(values = c("REED_Sephadex"="brown3",
+                                  "REED_NotSephadex"="cyan3",
+                                  "UTEX"="dodgerblue3"))
+  
+  ggsave(file.path(out_dir, paste0("coverage_", chr, ".pdf")), p, width = 20, height = 10)
+}
