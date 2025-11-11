d16c5f1d0154135847663d2c8f303d2538d00d80
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-10 21:12:29 -0500

    debugging

--- copy_number_variants_analysis.R
+++ copy_number_variants_analysis.R
@@ -11,11 +11,12 @@ library(doMC)
 registerDoMC(10)
 
 out_dir <- "/scratch/ejy4bu/compBio/bam_analysis/coverage_plots"
+in_dir <- "/scratch/ejy4bu/compBio/bam_analysis/coverage_data"
 metadata_file <- "/scratch/ejy4bu/compBio/bam_analysis/metadata.csv"
 
 meta <- fread(metadata_file)
 
-coverage_files <- list.files(out_dir, pattern = "_5000bp.csv", recursive = TRUE, full.names = TRUE)
+coverage_files <- list.files(in_dir, pattern = "_5000bp.csv", recursive = TRUE, full.names = TRUE)
 
 coverage <- rbindlist(lapply(coverage_files, function(f) {
   dt <- fread(f)
