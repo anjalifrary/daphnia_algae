fcb0976376d15c731474ac8cde914f0b5f10db49
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-17 09:16:49 -0500

    threshold 5

--- copy_number_variants_analysis.R
+++ copy_number_variants_analysis.R
@@ -62,11 +62,11 @@ for(chr in chr_list){
   # Create numeric x-axis along the chromosome (window index)
   setorder(chr_data, start)
   chr_data[, window_mid := (start + end)/2]  
-  chr_data[norm_depth > 20, norm_depth := 20]  
+  chr_data[norm_depth > 5, norm_depth := 5]  
 
-  dir.create(file.path(out_dir, "threshold20"), showWarnings=FALSE,recursive=TRUE)
+  dir.create(file.path(out_dir, "threshold5"), showWarnings=FALSE,recursive=TRUE)
 
-  chr_plot <- file.path(out_dir, "threshold20", paste0("Coverage_", chr, "_threshold20.pdf"))
+  chr_plot <- file.path(out_dir, "threshold5", paste0("Coverage_", chr, "_threshold5.pdf"))
   pdf(chr_plot, width=20, height=10)
   print(ggplot(chr_data, aes(x = window_mid, y = norm_depth, color = algae_group, group = sampleID)) +
     geom_line() +
