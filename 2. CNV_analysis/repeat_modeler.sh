d3767dcd3776e2b3a5468932e8fe2184c4c0f025
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-23 17:17:12 -0500

    running repeat modeler

--- 2. CNV_analysis/repeat_modeler.sh
+++ 2. CNV_analysis/repeat_modeler.sh
@@ -1,25 +1,38 @@
-
-
-
+#!/usr/bin/env bash
+#
+#SBATCH -J repeatModeler 
+#SBATCH --cpus-per-task=10
+#SBATCH --ntasks=1
+#SBATCH -N 1 # on one node
+#SBATCH -t 5-00:00 # 5 days
+#SBATCH --mem 150G
+#SBATCH -o /scratch/ejy4bu/err_outs/repeatModeler/repeatModeler.%A_%a.out # Standard output
+#SBATCH -e /scratch/ejy4bu/err_outs/repeatModeler/repeatModeler.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
 
 # make conda environment with repeatmodeler in it
 
 #conda create -n repeatmodeler_env -c bioconda -c conda-forge repeatmodeler perl=5.22.0
 conda activate repeatmodeler_new
-
-#update
-wd="/scratch/rjp5nc/removedups/us_dpulex/"
-cd ${wd}
+module load blast
 
 
-#ref="/scratch/rjp5nc/Reference_genomes/orig_ref/us_pulex_ref_GCF_021134715.1_ASM2113471v1_genomic.fna"
-#cleaned="/scratch/rjp5nc/removedups/us_dpulex/cleaned_US_pulex.fasta"
+proj="/project/berglandlab/chlorella_sequencing/reference_genome/GCA_023343905.1_cvul_genomic.fa"
+out_dir="/scratch/ejy4bu/compBio/cnv/reference"
+mkdir -p $out_dir
 
-#seqtk seq $ref > $cleaned
+# cp $proj $out_dir
+ref="${out_dir}/GCA_023343905.1_cvul_genomic.fa"
+cleaned="${out_dir}/GCA_023343905.1_cvul_genomic.cleaned.fasta"
+
+# cd $out_dir
+
+seqtk seq $ref > $cleaned
 
 #make blast db from cleaned reference genome (type nucleic acid)
-#makeblastdb -in $cleaned -out my_db -dbtype nucl -title my_db -parse_seqids
+makeblastdb -in $cleaned -out chlorellaDB -dbtype nucl -title chlorellaDB -parse_seqids
 
-#BuildDatabase -name my_db $cleaned
+# BuildDatabase -name clorellaDB $ref
 
-RepeatModeler -database my_db -pa 10
+RepeatModeler -database chlorellaDB -pa 10 -engine ncbi
