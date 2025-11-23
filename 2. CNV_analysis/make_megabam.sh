92703cdec8cbffdf55050cccb88102e569b2bd0c
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-23 14:04:29 -0500

    updating for all groups

--- 2. CNV_analysis/make_megabam.sh
+++ 2. CNV_analysis/make_megabam.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J megabam 
-#SBATCH --cpus-per-task=4
+#SBATCH --array=1-3
+#SBATCH --cpus-per-task=10
 #SBATCH --ntasks=1
 #SBATCH -N 1 # on one node
 #SBATCH -t 0-10:00 # 10 hours
@@ -16,9 +17,22 @@ module load samtools
 out_dir="/scratch/ejy4bu/compBio/cnv/megabams"
 mkdir -p $out_dir
 
-group="REED_NotSephedex"
-in_bams="/scratch/ejy4bu/compBio/cnv/data_tables/bam_paths/${group}_bamList.txt"
+groups=(/scratch/ejy4bu/compBio/cnv/data_tables/bam_paths/*_bamList.txt)
+bam_list=${groups[$SLURM_ARRAY_TASK_ID-1]}
+group=$(basename $bam_list _bamList.txt)
+#in_bams="/scratch/ejy4bu/compBio/cnv/data_tables/bam_paths/${group}_bamList.txt"
 out_bam="${out_dir}/${group}.megabam.bam"
 
-samtools merge -f -@ 10 $out_bam -b $in_bams 
+samtools merge -f -@ 10 $out_bam -b $bam_list
 samtools index $out_bam 
+
+echo "Done merging $group"
+
+
+# megabam="/scratch/ejy4bu/compBio/cnv/megabams/REED_NotSephedex.megabam.bam"
+# samtools quickcheck -v $megabam
+# samtools view -H $megabam | head
+# samtools view -c $megabam
+# samtools idxstats $megabam
+# samtools view $megabam
+# samtools flagstat $megabam
