d0fd3c3e70c419184a4a43551a26b85f55ceff6d
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-24 21:45:47 -0500

    updates

--- 2. CNV_analysis/megabam_coverage.R
+++ 2. CNV_analysis/megabam_coverage.R
@@ -16,21 +16,59 @@ registerDoMC(10)
 
 
 ### Define megabam paths
-#megabams <- system("ls -d /scratch/ejy4bu/compBio/cnv/megabams/*.megabam.bam", intern=T)
-megabam <- "/scratch/ejy4bu/compBio/cnv/megabams/*.megabam.bam"
+megabams <- system("ls -d /scratch/ejy4bu/compBio/cnv/megabams/*.megabam.bam", intern=T)
+#megabam <- "/scratch/ejy4bu/compBio/cnv/megabams/*.megabam.bam"
+message("Found ", length(megabams), " megabam files")
+
+out_dir <- "/scratch/ejy4bu/compBio/cnv/megabamCoverageAnalysis"
+dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
+
 
 ### Set chlorella scaffolds
     chr <- c("SIDB01000001.1","SIDB01000002.1","SIDB01000003.1","SIDB01000004.1",
     "SIDB01000005.1","SIDB01000006.1","SIDB01000007.1","SIDB01000008.1","SIDB01000009.1",
     "SIDB01000010.1","SIDB01000011.1","SIDB01000012.1","SIDB01000013.1","SIDB01000014.1")
 
-bamfile <- BamFile(megabam)
-open(bamfile)
 
-out_dir <- "/scratch/ejy4bu/compBio/cnv/megabamCoverageAnalysis"
-dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
+# get chromosome lengths from scaffold file
+chrom_lengths <- fread("/scratch/ejy4bu/compBio/genomefiles/scaffold_lengths.txt",
+                       col.names = c("chr_names", "chr_lengths"))
+setkey(chrom_lengths, chr_names)
+
+# get read length
+bam <- scanBam(megabams[1])
+read_length <- mean(nchar(as.character(bam[[1]]$seq)))
+
+# calculate coverage per megaBAM per chromosome
+coverage <- foreach(bamFile = megabams, .combine="rbind") %dopar% {
+    message("Processing: ", bamFile)
+
+    if(!file.exists(paste(bamFile, ".bai", sep=""))) indexBam(bamFile)
+    
+    # get mapped reads per chromosome 
+    stats <- as.data.table(idxstatsBam(bamFile))
+    setnames(stats, "seqnames", "chr_names")  # rename
+    setkey(stats, chr_names)
+
+    # subset to chlorella chromosomes 
+    stats_pulex <- stats[J(chr)]
+
+    # merge with chromosome lengths
+    stats_pulex <- merge(stats_pulex, chrom_lengths, by="chr_names")
+
+    # calculate coverage: (mapped reads * read length) / chromosome length
+    stats_pulex[, coverage := (mapped * read_length) / chr_lengths]
+    
+    # add sample ID
+    stats_pulex[, sampleID := sub(".megabam.bam", "", basename(bamFile))]
+
+    stats_pulex[, .(sampleID, chr_names, mapped, chr_lengths, coverage)]
+}
 
 
+
+# data table
+out_file <- file.path(out_dir, "megabam_coverage_analysis.pdf")
 
 ### per chromosome plots
 chr_list <- unique(coverage$chrom)
@@ -43,14 +81,13 @@ for(chr in chr_list){
   chr_data[, window_mid := (start + end)/2]  
 
   ### calculate group mean coverage
-#   chr_group_data <- chr_data[, .(
-#     mean_norm_depth = mean(norm_depth, na.rm=TRUE)
-#     ), by = .(window_mid, algae_group)]
+  chr_group_data <- chr_data[, .(
+    mean_norm_depth = mean(norm_depth, na.rm=TRUE)
+    ), by = .(window_mid, algae_group)]
 
   #chr_data[norm_depth > 5, norm_depth := 5]  
   #chr_group_data[mean_norm_depth > 5, mean_norm_depth := 5]
 
-  #dir.create(file.path(out_dir), showWarnings=FALSE,recursive=TRUE)
 
   chr_plot <- file.path(out_dir, paste0("Coverage_", chr, ".pdf"))
   pdf(chr_plot, width=20, height=10)
--- 2. CNV_analysis/repeat_modeler.sh
+++ 2. CNV_analysis/repeat_modeler.sh
@@ -17,9 +17,6 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate repeatmodeler_env
-# conda install -c bioconda blast
-
-# module load blast
 
 # proj="/project/berglandlab/chlorella_sequencing/reference_genome/GCA_023343905.1_cvul_genomic.fa"
 out_dir="/scratch/ejy4bu/compBio/cnv/reference"
@@ -28,10 +25,10 @@ out_dir="/scratch/ejy4bu/compBio/cnv/reference"
 # cp $proj $out_dir
 # ref="${out_dir}/GCA_023343905.1_cvul_genomic.fa"
 cleaned="${out_dir}/GCA_023343905.1_cvul_genomic.cleaned.fasta"
+# seqtk seq $ref > $cleaned
 
 cd $out_dir
 
-# seqtk seq $ref > $cleaned
 ls -lh $cleaned
 
 
@@ -40,3 +37,36 @@ makeblastdb -in $cleaned -out my_db -dbtype nucl -title my_db -parse_seqids
 BuildDatabase -name my_db $cleaned
 
 RepeatModeler -database my_db -pa 10
+
+
+
+# make index and produce scaffold length file
+out_dir="/scratch/ejy4bu/compBio/cnv/reference"
+cleaned="${out_dir}/GCA_023343905.1_cvul_genomic.cleaned.fasta"
+
+module load samtools
+mkdir -p "scratch/ejy4bu/compBio/cnv/megabams/debug"
+scaffold_lengths="scratch/ejy4bu/compBio/cnv/megabams/debug/scaffold_lengths.txt"
+samtools faidx "$cleaned"
+
+# use the .fai to get scaffold names + lengths
+fai="${cleaned}.fai"
+
+cut -f1,2 "$fai" > "$scaffold_lengths"
+
+# quick checks
+wc -l "$scaffold_lengths"            # number of scaffolds
+head -n 10 "$scaffold_lengths"       # first 10 scaffolds with lengths
+tail -n 10 "$scaffold_lengths"       # last 10 scaffolds with lengths
+
+# total bases
+awk '{s+=$2} END{print "total_bp="s}' "$scaffold_lengths"
+
+cut -f2 $fai | sort -nr > "scratch/ejy4bu/compBio/cnv/megabams/debug/lengths.txt"
+awk 'BEGIN{sum=0; i=0} {sum+=$1; a[i++]=$1; total+= $1} END{half=total/2; s=0; for(j=0;j<i;j++){s+=a[j]; if(s>=half){print "N50="a[j]; break}} }' "scratch/ejy4bu/compBio/cnv/megabams/debug/lengths.txt"
+
+consensi="/sfs/weka/scratch/ejy4bu/compBio/cnv/reference/RM_175000.SunNov231803172025/consensi.fa"
+
+# change path to the actual consensi file found above
+grep -c '^>' $consensi
+grep -c '^>' $consensi.classified   # if present
