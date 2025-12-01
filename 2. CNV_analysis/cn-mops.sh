b2130ca5e2d0ac638f1836bc34b27e348d570528
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-30 22:27:56 -0500

    .

--- 2. CNV_analysis/cn-mops.sh
+++ 2. CNV_analysis/cn-mops.sh
@@ -4,10 +4,12 @@
 #SBATCH -N 1
 #SBATCH -t 0-10:00 # 10 hours
 #SBATCH --mem=100G
-#SBATCH -o /scratch/ejy4bu/erroroutputs/cnmops/cnmops.%A.out
-#SBATCH -e /scratch/ejy4bu/erroroutputs/cnmops/cnmops.%A.err
+#SBATCH -o /scratch/ejy4bu/err_outs/cnmops/cnmops.%A.out
+#SBATCH -e /scratch/ejy4bu/err_outs/cnmops/cnmops.%A.err
 #SBATCH -p standard
 #SBATCH --account berglandlab
+
+mkdir -p /scratch/ejy4bu/err_outs/cnmops
 
 export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
 module load gcc/11.4.0 openmpi/4.1.4 icu R/4.5.0
