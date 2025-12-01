5de8b2c954329fe54846bc22d556a534790dfa4f
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-30 22:29:22 -0500

    .

--- 2. CNV_analysis/cn-mops.sh
+++ 2. CNV_analysis/cn-mops.sh
@@ -13,6 +13,7 @@ mkdir -p /scratch/ejy4bu/err_outs/cnmops
 
 export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
 module load gcc/11.4.0 openmpi/4.1.4 icu R/4.5.0
+
 module load R/4.5.0
 
-Rscript cn-mops.R
+Rscript 2.\ CNV_analysis/cn-mops.R
