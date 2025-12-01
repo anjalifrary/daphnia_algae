c91e01808157e494496f58f068244608ed0b1b96
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-11-30 22:45:19 -0500

    .

--- 2. CNV_analysis/cn-mops.R
+++ 2. CNV_analysis/cn-mops.R
@@ -31,6 +31,8 @@ UTEX_bams <- c("/scratch/ejy4bu/compBio/cnv/megabams/UTEX.megabam.bam")
 bam_files <- c(sephadex_bams, REED_bams, UTEX_bams)
 sample_names <- c("REED_Sephadex", "REED_NotSephadex", "UTEX")
 
+message("Input bams set")
+
 
 ### get input bam lengths
 
@@ -38,6 +40,8 @@ sample_names <- c("REED_Sephadex", "REED_NotSephadex", "UTEX")
     chr <- c("SIDB01000001.1","SIDB01000002.1","SIDB01000003.1","SIDB01000004.1",
     "SIDB01000005.1","SIDB01000006.1","SIDB01000007.1","SIDB01000008.1","SIDB01000009.1",
     "SIDB01000010.1","SIDB01000011.1","SIDB01000012.1","SIDB01000013.1","SIDB01000014.1")
+
+message("Scaffolds set")
 
 
 # should i specify window length? WL = ?? 1 kb windows?
@@ -58,14 +62,14 @@ res <- cn.mops(bamDataRanges, minWidth=2, parallel=4)
 
 
 pdf(file.path(out_dir, "cnmops_rawOutput.pdf"), width=14,height=8)
-plot(res)
+plot(res, which=1-3)
 dev.off()
-message("Raw depth-read values after normalization plot saved to: ", paste0(out_dir, "cnmops_rawOutput.pdf"))
+message("Raw depth-read values after normalization plot saved.")
 
 ### Calculate CN values
 cnv_result <- calcIntegerCopyNumbers(res)
 
-cnv_df <- as.data.frame(cnvToGRanges(cnv_result))
+cnv_df <- as.data.frame(cnvr(cnv_result))
 cnv_df <- cnv_df[, c("sampleName", setdiff(colnames(cnv_df), "sampleName"))]
 fwrite(cnv_df, file.path(out_dir, "cnmops_CNVs.csv"), sep=",", quote=FALSE)
 message("CNV calls saved to ", paste0(out_dir, "cnmops_CNVs.csv"))
