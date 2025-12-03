720f0182722451a453fea2c5f0b6aad4170b103d
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-12-02 20:47:48 -0500

    ,

--- 2. CNV_analysis/coverage_plot.R
+++ 2. CNV_analysis/coverage_plot.R
@@ -132,7 +132,7 @@ coverage_plot <- file.path(out_dir, "avg_coverage_per_chromosome.pdf")
   print(
   ggplot(coverage_avg,
       aes(x=chr_names, y = coverage)) + 
-      geom_bar(stat="identity", width = 0.7, fill="dodgerblue3") +
+      geom_bar(stat="identity", width = 0.7, fill="lightblue") +
       ylab("Average Coverage") + 
       xlab("Scaffold") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1, size=14),
