02ddb5fbffa3467f7062f28f2d7a36bb769e1694
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-12-02 20:08:52 -0500

    memory

--- 2. CNV_analysis/coverage_plot.R
+++ 2. CNV_analysis/coverage_plot.R
@@ -1,4 +1,4 @@
-#ijob -A berglandlab -c2 -p standard --mem=40G
+#ijob -A berglandlab -c4 -p standard --mem=40G
 #export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
 #module load gcc/11.4.0  openmpi/4.1.4 icu R/4.5.0
 #module load R/4.5.0
@@ -9,7 +9,7 @@ library(data.table)
 library(ggplot2)
 library(foreach)
 library(doMC)
-registerDoMC(10)
+registerDoMC(4)
 
 
 
