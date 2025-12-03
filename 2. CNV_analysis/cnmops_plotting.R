b1c673f3233bc5b0c87f380bd5da2b36914bad7b
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2025-12-02 22:03:45 -0500

    again

--- 2. CNV_analysis/cnmops_plotting.R
+++ 2. CNV_analysis/cnmops_plotting.R
@@ -24,32 +24,31 @@ colnames(cn_data)[colnames(cn_data) == "REED_NotSephadex"] <- "REED"
 # cn_cont$chrom <- as.character(seqnames(bamDataRanges))
 # cn_cont$pos <- start(bamDataRanges)
 
-
+fwrap_size <- 1000  # adjust based on scaffold length
 
 for(sc in unique(cn_data$chrom)) {
   sc_data <- cn_data[cn_data$chrom == sc, ]
   
+  # each line group  for wrapping
+  sc_data$line <- ceiling(seq_len(nrow(sc_data)) / wrap_size)
+  
   # File path for this scaffold
-  pdf_file <- file.path(out_dir, paste0("cnmops_", sc, "integerCN.pdf"))
+  pdf_file <- file.path(out_dir, paste0("cnmops_", sc, "_wrapped.pdf"))
   pdf(pdf_file, width=20, height=10)
   
-  # Plot UTEX and REED cn 
-  # green line is reference (normalized at 1 for haploid chlorella)
-  # each scaffold is a gray bar 
-
-  cn_plot <- ggplot(cn_plot, aes(x=pos)) +
+  ggplot(sc_data, aes(x=pos)) +
     geom_line(aes(y=REED, color="REED"), linewidth=1.2) +
     geom_line(aes(y=UTEX, color="UTEX"), linewidth=1.2) +
-    facet_wrap(~chrom, scales="free_x", ncol=1) +  # one column, scaffolds stacked vertically
+    facet_wrap(~line, scales="free_x", ncol=1) +  # each line is a section of the scaffold
     scale_color_manual(values=c("REED"="cyan3", "UTEX"="dodgerblue3")) +
     scale_y_continuous(breaks=scales::pretty_breaks(n=5)) +
     theme_bw() +
     theme(
-      legend.position="top",
       axis.text.x = element_text(angle=45, hjust=1, size=14),
       axis.text.y = element_text(size=14),
       axis.title.x = element_text(size=16),
       axis.title.y = element_text(size=16),
+      legend.position="top",
       strip.background = element_rect(fill="lightgray", color="black"),
       strip.text = element_text(face="bold", size=14)
     ) +
@@ -59,8 +58,7 @@ for(sc in unique(cn_data$chrom)) {
       color = "Sample",
       title = paste("cn.mops Integer Copy Number -", sc)
     )
-
-  print(cn_plot)
+  
   dev.off()
-  message(paste0("Saved PDF for scaffold ", sc, ": ", pdf_file))
+  message(paste0("Saved wrapped PDF for scaffold ", sc, ": ", pdf_file))
 }
